0438c3a71a0647537688f6d7f0850813e0d9b4ec
Greg Schuman <gcschuman@hotmail.com>
2020-06-15 18:26:43 -0700

    Added function renderLicenseSelection

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,9 +3,20 @@ function generateProjectUrl(github, title){
     return `https://github.com/${github}/${kebabCaseTitle}`;
 }
 
-function renserLicenseBadge(license, github, title){
+function renderLicenseBadge(license, github, title){
     if(license !== "None"){
         return `[![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)](${generateProjectUrl(github, title)})`
+    }
+    return ''
+}
+
+function renderLicenseSelection(licence){
+    if(license !== "None"){
+        return(
+          `## License
+          
+          This project is licensed under the ${license} license`  
+        )
     }
     return ''
 }
