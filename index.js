fed02eff45146dfd43a486c382c71cc69510e760
Greg Schuman <gcschuman@hotmail.com>
2020-06-15 19:57:24 -0700

    Added 'what is your GitHub username and project name questions.

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const inquirer = require("inquirer");
 const api = require("./utils/api");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?"
+  }
+]
 
 
 
