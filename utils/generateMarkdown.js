c58800d0ff7b549e7b9ec4de36ca6b89bcf75f59
Greg Schuman <gcschuman@hotmail.com>
2020-06-15 18:14:23 -0700

    Added function renderLicenseBadge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,3 +2,11 @@ function generateProjectUrl(github, title){
     const kebabCaseTitle = title.toLowerCase().split(" ").join("-");
     return `https://github.com/${github}/${kebabCaseTitle}`;
 }
+
+function renserLicenseBadge(license, github, title){
+    if(license !== "None"){
+        return `[![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)](${generateProjectUrl(github, title)})`
+    }
+    return ''
+}
+
